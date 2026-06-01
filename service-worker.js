92553d510ca8c3ee0494776018cfe39bf1bb0cf6
trustindestiny <96923968+trustindestiny@users.noreply.github.com>
2026-06-01 16:45:31 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-
-const CACHE='jpy-try-v1';
-self.addEventListener('install', e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./icon-192.png','./icon-512.png'])));
-});
-self.addEventListener('fetch', e=>{
- e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
-});
